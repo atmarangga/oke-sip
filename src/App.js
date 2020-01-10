a3fc0cf2b2b0784e06247d81ea00ff0c671e4cb3
Atmaji Rangga Adinugroho <atmaji.rangga@gmail.com>
2020-01-10 11:10:44 +0700

    done editing package and adding plugin

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "semantic-ui-css/semantic.min.css";
+import { Button } from "semantic-ui-react";
+// import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">        
-      </header>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        paddingTop: 0,
+        paddingBottom: 0,
+        margin: 0,
+        backgroundColor: '#ffaa0a'
+      }}
+    >
+      <div>
+        <Button primary>Test</Button>
+      </div>
     </div>
   );
 }
